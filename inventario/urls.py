32f0001c5972b1c5c1d5fd188fc46b9b046ef12b
joacogaarcia01 <joacogarcia01@gmail.com>
2019-12-06 01:43:37 -0300

    mas cambios

--- inventario/urls.py
+++ inventario/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from .views import index, item_list
+from .views import create_lab, index, item_list, create_object
 
 urlpatterns = [
     path('', index, name='home'),
+    path('test/', create_lab, name='create_lab'),
+    path('objecto/', create_object, name='create_object'),
     path('lab/<int:lab_id>/', item_list, name='item_list'),
+
 ]
--- inventario/views.py
+++ inventario/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from .models import Item, Lab
 
@@ -10,5 +10,19 @@ def index(request):
 
 def item_list(request, lab_id):
     items = Item.objects.filter(id_lab_id=lab_id)
-    ctx = {'items': items}
+    ctx = {'items': items, "lab_id": lab_id}
     return render(request, 'item/list.html', ctx)
+
+def create_lab(request):
+    name = request.POST['name']
+    lab = Lab(name=name)
+    lab.save()
+    return redirect('/')
+
+def create_object(request):
+    name = request.POST['name']
+    lab_id = request.POST["lab_id"]
+    lab = Lab.objects.get(pk= lab_id)
+    obj = Item(name=name, id_lab=lab)
+    obj.save()
+    return redirect('/')
