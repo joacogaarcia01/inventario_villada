158499ba22fbf79699ccde7e0305471bf137276e
joacogaarcia01 <joacogarcia01@gmail.com>
2019-12-06 22:43:38 -0300

    mas cambios

--- inventario/urls.py
+++ inventario/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from .views import create_lab, index, item_list, create_object
+from .views import Index, LabDelete, item_list, create_object
 
 urlpatterns = [
-    path('', index, name='home'),
-    path('test/', create_lab, name='create_lab'),
+    path('', Index.as_view(), name='index'),
+    path('delete/<int:lab_id>/', LabDelete.as_view(), name='lab_delete'),
     path('objecto/', create_object, name='create_object'),
     path('lab/<int:lab_id>/', item_list, name='item_list'),
-
 ]
--- inventario/views.py
+++ inventario/views.py
@@ -1,23 +1,38 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
+from django.views import View
 
 from .models import Item, Lab
 
-def index(request):
-    labs = Lab.objects.all()
-    ctx = {'labs': labs}
-    return render(request, 'index.html', ctx)
+
+class Index(View):
+
+    def get(self, request):
+        print(request.method)
+        labs = Lab.objects.all()
+        ctx = {'labs': labs}
+        return render(request, 'index.html', ctx)
+
+    def post(self, request):
+        name = request.POST['name']
+        lab = Lab(name=name)
+        lab.save()
+        return redirect('index')
+
+
+class LabDelete(View):
+
+    def get(self, request, *args, **kwargs):
+        lab_id = kwargs['lab_id']
+        lab = Lab.objects.get(pk=lab_id)
+        lab.delete()
+        return redirect('index')
+
 
 def item_list(request, lab_id):
     items = Item.objects.filter(id_lab_id=lab_id)
     ctx = {'items': items, "lab_id": lab_id}
     return render(request, 'item/list.html', ctx)
-
-def create_lab(request):
-    name = request.POST['name']
-    lab = Lab(name=name)
-    lab.save()
-    return redirect('/')
 
 def create_object(request):
     name = request.POST['name']
